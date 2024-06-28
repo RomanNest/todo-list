95a7c913a083e12fb250b608c84cd8b17913d68e
Roma Nesterenko <rom.puh@gmail.com>
2024-06-28 12:08:14 +0300

    create views for Tag

--- todo_service/views.py
+++ todo_service/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.urls import reverse_lazy
 from django.views import generic
 
 from todo_service.models import Task, Tag
@@ -9,4 +10,22 @@ class HomePageView(generic.ListView):
     template_name = "todo_service/index.html"
 
 
-# Create your views here.
+class TagListView(generic.ListView):
+    model = Tag
+
+
+class TagCreateView(generic.CreateView):
+    model = Tag
+    fields = "__all__"
+    success_url = reverse_lazy("todo_service:tag-list")
+
+
+class TagUpdateView(generic.UpdateView):
+    model = Tag
+    fields = "__all__"
+    success_url = reverse_lazy("todo_service:tag-list")
+
+
+class TagDeleteView(generic.DeleteView):
+    model = Tag
+    success_url = reverse_lazy("todo_service:tag-list")
