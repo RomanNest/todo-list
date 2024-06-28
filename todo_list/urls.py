25928f996de4fbc0093d47514726b11423bf2b6d
Roma Nesterenko <rom.puh@gmail.com>
2024-06-28 11:04:30 +0300

    add path to todo_service in todo_list.urls

--- todo_list/urls.py
+++ todo_list/urls.py
@@ -15,9 +15,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-]
+    path("", include("todo_service.urls", namespace="todo_service")),
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
