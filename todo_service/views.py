5f4aec7dae4c6446570eb5f98ed1561bc91319bb
Roma Nesterenko <rom.puh@gmail.com>
2024-06-28 11:42:33 +0300

    create view for HomePage

--- todo_service/views.py
+++ todo_service/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.views import generic
+
+from todo_service.models import Task, Tag
+
+
+class HomePageView(generic.ListView):
+    model = Task
+    template_name = "todo_service/index.html"
+
 
 # Create your views here.
