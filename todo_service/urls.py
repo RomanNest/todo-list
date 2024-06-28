f20c9fdd8ac4e8c5ea74d03757445af63b6320e1
Roma Nesterenko <rom.puh@gmail.com>
2024-06-28 12:04:01 +0300

    add paths to Tag in todo_service.urls

--- todo_service/urls.py
+++ todo_service/urls.py
@@ -2,11 +2,35 @@ from django.urls import path
 
 from todo_service.views import (
     HomePageView,
+    TagListView,
+    TagCreateView,
+    TagUpdateView,
+    TagDeleteView,
+
 )
 
 
 urlpatterns = [
     path("", HomePageView.as_view(), name="index"),
+    path(
+        "tags/",
+        TagListView.as_view(),
+        name="tag-list",
+    ),
+    path("tags/create/",
+         TagCreateView.as_view(),
+         name="tags-create",
+         ),
+    path(
+        "tags/<int:pk>/update/",
+        TagUpdateView.as_view(),
+        name="tags-update",
+    ),
+    path(
+        "tags/<int:pk>/delete/",
+        TagDeleteView.as_view(),
+        name="tags-delete",
+    ),
     ]
 
 app_name = "todo_service"
